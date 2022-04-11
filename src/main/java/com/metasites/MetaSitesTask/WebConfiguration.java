fa806644b73160db54b801b00db7f91e0e8b005d
MindeB <MindaugasBer@hotmail.lt>
2022-04-11 15:38:52 +0300

    Added Angular packaged project to resources folder

--- src/main/java/com/metasites/MetaSitesTask/WebConfiguration.java
+++ src/main/java/com/metasites/MetaSitesTask/WebConfiguration.java
@@ -9,6 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("/properties/**")
+                .addResourceLocations("file:/env/");
+    }
+
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/swagger", "/swagger-ui.html");
     }
